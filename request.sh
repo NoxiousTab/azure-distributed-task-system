a43a6030dd1a02dbb1c7d355768ba66babae79a4
Ahmed Tabish <noxioustab@gmail.com>
2025-11-23 19:32:05 +0530

    cleanup

--- request.sh
+++
@@ -1,3 +0,0 @@
-curl -X POST "http://localhost:5000/submit-task" ^
-  -H "Content-Type: application/json" ^
-  -d "{\"type\":\"summarize\",\"text\":\"This is a long piece of text that we want to summarize using the local Azure distributed task system.\"}"
